0075f4fc0e6f5fa662b5479fea80fee8e4534c4d
dav-dev-codes <davilopesr2244@gmail.com>
2025-06-02 19:26:12 -0300

    feat: Biblioteca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products ');
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
 
-export default function Home() {
+  return res.json();
+}
+export default async function Home() {
+  const products = await getProducts();
   return (
    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
-    <div className="grid">Block Store
-      
+    <div className="grid grid-cols-1 sm:grid-cols-2 mdgrid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+      {products.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ) )}
     </div>
    </div>
 
